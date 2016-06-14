cee7a473986bb898fe30470ebfb9667c8b09f30a
MichaelPekar <pekar.misha@gmail.com>
2016-06-14 14:29:40 +0300

    Restructured_cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,13 @@
 class StoreController < ApplicationController
+    include CurrentCart
+   before_action :set_cart
+
   def index
     @products = Product.order(:title)
-    if session[:counter]
-      session[:counter] += 1
-    else
-      session[:counter] = 1
-    end
+    # if session[:counter]
+    #   session[:counter] += 1
+    # else
+    #   session[:counter] = 1
+    # end
   end
 end
